88c002239e3104d75afe853301f1f344e47459b3
Wijai Thongsom <wijai.thongsom@gmail.com>
2025-03-15 16:11:16 +0700

    Use app variable name

--- CICD.sh
+++ CICD.sh
@@ -4,9 +4,9 @@ set -e
 APP_NAME=mywebapp
 VERSION=INSECURE
 
-#BUILD: Clenaup old container/build image
+#BUILD: Clean up old container/build image
 docker rm -f $APP_NAME 2>/dev/null || true
 docker build -t $APP_NAME:$VERSION . 
 
 #TEST: Run the container
-docker run -d -p 80:80 --name webapp $APP_NAME:$VERSION 
+docker run -d -p 80:80 --name $APP_NAME $APP_NAME:$VERSION 
